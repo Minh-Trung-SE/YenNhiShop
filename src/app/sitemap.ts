5712f1842c5a09129915b5700ecbfeea7aa53064
Minh Trung <nguyenminhtrungse@gmail.com>
2024-09-30 19:25:39 +0700

    Update SSL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://yennhi.com',
-            lastModified:'2024-09-29',
+            url: 'https://banhyennhi.com',
+            lastModified:'2024-09-30',
             priority: 1,
         },
         {
-            url: 'https://yennhi.com/cau-chuyen-san-pham',
-            lastModified:'2024-09-29',
+            url: 'https://banhyennhi.com/cau-chuyen-san-pham',
+            lastModified:'2024-09-30',
             priority: 1,
         },
     ]
